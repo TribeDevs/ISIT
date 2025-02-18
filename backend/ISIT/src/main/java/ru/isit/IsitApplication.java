66b67c5f2ceadc7d25d1aae85ddc49f03ada2b53
Андрей <111282657+YaroGITk@users.noreply.github.com>
2025-02-18 20:11:22 +0700

    Create spring app

---
+++ backend/ISIT/src/main/java/ru/isit/IsitApplication.java
@@ -0,0 +1,13 @@
+package ru.isit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsitApplication.class, args);
+    }
+
+}
