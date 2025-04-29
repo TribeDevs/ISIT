77c4daa47b6dd68573760787fb086d8f5fcad5ae
Andrey <yaroshenko_andrey2006@mail.ru>
2025-04-29 18:09:43 +0700

    add dto change password

---
+++ backend/ISIT/src/main/java/ru/isit/dto/request/ChangePassword.java
@@ -0,0 +1,19 @@
+package ru.isit.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ChangePassword {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String lastPassword;
+
+    @Size(min = 8, max = 100)
+    private String newPassword;
+
+}
